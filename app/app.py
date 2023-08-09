2d8167af5af21510e8a1dfec2f01f213ee75d820
bensocho <bensocho10@gmail.com>
2023-08-10 01:27:55 +0300

    Initial commit, application runs locally with docker compose and without a ci/cd pipeline

---
+++ app/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+import os
+
+mongo_uri = os.environ.get("MONGO_URI")
+print("Connecting to MongoDB using URI:", os.environ.get("MONGO_URI"))
+
+app = Flask(__name__, template_folder='/app/templates')
+
+client = MongoClient(mongo_uri)
+db = client.my_flask_db
+fruits_collection = db.fruits
+
+
+
+@app.route('/')
+def apples():
+    apples_data = fruits_collection.find_one({"name": "apples"})
+    if apples_data:
+        apples_qty = apples_data.get("qty")
+    else:
+        apples_qty = "N/A"
+    
+    return render_template('index.html', apples_qty=apples_qty)
+    
+app.run(host="0.0.0.0")
+
---
+++ db/init-data.js
@@ -0,0 +1,22 @@
+var conn = new Mongo();
+var db = conn.getDB("my_flask_db");
+
+var adminUser = db.getUsers({ user: "admin" });
+if (adminUser.length === 0) {
+  db.createUser({
+    user: "admin",
+    pwd: "password",
+    roles: [{ role: "readWrite", db: "my_flask_db" }]
+  });
+}
+
+db.createCollection("fruits");
+db.fruits.insertMany([
+  { "_id": 1, "name": "apples", "qty": 5, "rating": 3 },
+  { "_id": 2, "name": "bananas", "qty": 7, "rating": 1, "microsieverts": 0.1 },
+  { "_id": 3, "name": "oranges", "qty": 6, "rating": 2 },
+  { "_id": 4, "name": "avocados", "qty": 3, "rating": 5 },
+]);
+
+print("Initialized database 'my_flask_db' with collection 'fruits'.");
+
