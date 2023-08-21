69b9cdde05d0742ffffe094c2f1638bd65ca9465
bensocho <bensocho10@gmail.com>
2023-08-21 21:41:56 +0300

    updated autoscaling.tf and sq.tf

--- terraform/autoscaling.tf
+++ terraform/autoscaling.tf
@@ -1,6 +1,6 @@
 resource "aws_appautoscaling_target" "ecs_target" {
-  max_capacity       = 2
-  min_capacity       = 1
+  max_capacity       = 5
+  min_capacity       = 2
   resource_id        = "service/${aws_ecs_cluster.apples-prod.name}/${aws_ecs_service.service.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
--- terraform/sg.tf
+++ terraform/sg.tf
@@ -4,6 +4,13 @@ resource "aws_security_group" "alb_sg" {
   ingress {
     from_port = 80
     to_port = 80
+    protocol = "http"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 5000
+    to_port = 5000
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -13,17 +20,10 @@ resource "aws_security_group" "apples_task_sg" {
   name_prefix = "apples_task_sg-"
   vpc_id = aws_vpc.little-apples-vpc.id
   ingress {
-    from_port = 80
+    from_port = 5000
     to_port = 5000
     protocol = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -31,14 +31,7 @@ resource "aws_security_group" "flask_sg" {
     name_prefix = "flask-sg-"
     vpc_id = aws_vpc.little-apples-vpc.id
     ingress {
-        from_port = 80
-        to_port = 5000
-        protocol = "tcp"
-        security_groups = [aws_security_group.alb_sg.id]
-    }
-
-    ingress {
-        from_port = 80
+        from_port = 5000
         to_port = 5000
         protocol = "tcp"
         security_groups = [aws_security_group.apples_task_sg.id]
@@ -53,13 +46,6 @@ resource "aws_security_group" "mongo_sg" {
         to_port     = 27017
         protocol    = "tcp"
         security_groups = [aws_security_group.flask_sg.id]
-    }
-
-     ingress {
-        from_port = 27017
-        to_port = 27017
-        protocol = "tcp"
-        security_groups = [aws_security_group.apples_task_sg.id]
     }
 }
 
